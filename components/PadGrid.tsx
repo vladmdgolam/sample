55888265bf4bbe3f9bbbd1e621668158200ec184
Vlad <vladmdgolam@gmail.com>
2026-01-25 00:56:53 +0700

    Update PadGrid.tsx

--- components/PadGrid.tsx
+++ components/PadGrid.tsx
@@ -444,7 +444,7 @@ export const PadGrid: React.FC<PadGridProps> = ({ padKeys, keyToPadMapping, isDa
               )} */}
             </div>
             <div className="text-center md:text-left text-[2.5vw] md:text-[0.75vw] text-[var(--c-text-dark)] opacity-70">
-              Samples by{" "}
+              808 Pack by{" "}
               <a
                 href="https://drumboii.com/products/free-808-sample-pack"
                 target="_blank"
